875cfb07692fb8dee398ad79d861fcf169d5f6d6
Mason Guiste <mguiste@live.com>
2017-10-17 08:39:59 -0700

    Finished exercise 1 and lab

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -5,19 +5,21 @@
                            # http://www.footballdb.com/teams/nfl/seattle-seahawks/results
                            # here 'Result' displays scores, 'L' stands for loss, 'W' for win
 
+pointsScored = c(9, 12, 27, 46, 16)
 # Create a vector of the number of points the Seahawks have allowed to be scored against them in the first 5 games
-
+pointsAllowed = c(17, 9, 33, 18, 10)
 
 # Combine your two vectors into a dataframe
-
+seahawks = data.frame(pointsScored, pointsAllowed)
 
 # Create a new column "diff" that is the difference in points
-
+seahawks$diff = pointsScored - pointsAllowed
 
 # Create a new column "won" which is TRUE if the Seahawks won
-
+seahawks$won = seahawks$diff > 0
 
 # Create a vector of the opponents
-
+opponents = c("Green Bay", "San Francisco", "Tenessee", "Indianapolis", "LA Rams")
 
 # Add the vector of opponents into the data frame
+seahawks$opponents = opponents
--- lab/exercise.R
+++ lab/exercise.R
@@ -1,25 +1,37 @@
 # Create a vector of 100 Credit Card Users
+user = 1:100
 
 # Create a vector of amount of money owed using the runif function
+owed = runif(100, 0, 100)
 
 # Create a vector of amount of money paid (that are typically higher than the amount Owed) (use runif again)
+paid = owed + runif(100, -50, 100)
 
 # Create a data.frame by combining the vectors
+bank_stuff = data.frame(user, owed, paid)
 
 # Create a column "Difference" that stores the difference between amount Paid and amount Owed
+bank_stuff$difference = bank_stuff$owed-bank_stuff$paid
 
 # Create a column that is TRUE if the person Paid more than they Owe
+bank_stuff$paid_Debt = bank_stuff$difference > 0
 
 # What was the amount Owed of User 57?
+bank_stuff[57, "owed"]
 
 # How many users paid more than they owe?
+paidMore = nrow(filter(bank_stuff, bank_stuff$difference>0))
 
 # What was the total amount of money paid for all users?
+totalPaid = sum(bank_stuff$paid)
 
 # What was the name of the user who had the highest "Difference"?
+highestDifference = bank_stuff[bank_stuff$difference == max(bank_stuff$difference), 1]
 
-# Who was the user with the largest amount paid? 
+# Who was the user with the largest amount paid?
+largestPaid = bank_stuff[bank_stuff$paid == max(bank_stuff$paid), 1]
 
 # What was the average "Difference"?
+avgDifference = mean(bank_stuff$difference)
 
 # challenge: how to use dplyr to answer these questions?
